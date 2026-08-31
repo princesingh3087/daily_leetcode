dc6aa4f3c4be068a7810370b3f815491e63c9d68
princesingh3087 <princesingh363626@gmail.com>
2026-09-01 03:35:01 +0530

    Time: 115 ms (32.76%), Space: 142.2 MB (6.38%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
             if(mp.find(nums[i])!=mp.end()){
-                if(i-mp[nums[i]]<=k){
+                if(abs(i-mp[nums[i]])<=k){
                     return true;
                 }
             }
